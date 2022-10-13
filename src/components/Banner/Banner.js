bff5eeb3b12d88f075a9a84e8e2314bcc9ce49a8
zLeggi <leandrosalmeida200021@gmail.com>
2022-10-13 13:26:48 -0300

    primeiro push do projeto

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import "./Banner.css";
+
+export const Banner = () => {
+  return (
+    <header className="banner">
+      <img src="/images/banner.png" alt="Banner principal da pagina organo" />
+    </header  >
+  );
+};
